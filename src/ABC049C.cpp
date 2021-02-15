e7b4b5244051817a6fb8b0d354af8e7d186b0b12
Spice-Z <jigenbakdaaan@gmail.com>
2021-02-16 00:10:35 +0900

    fix init

---
+++ src/ABC049C.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(string &target)
+{
+
+  const vector<string> candidates = {"dreamer", "eraser", "dream", "erase"};
+  for (int i = 0; candidates.size();)
+  {
+    // target切り取り
+    candidates.at(i);
+
+    // check(target)
+  }
+
+  if (target.size() == 0)
+  {
+    return true;
+  }
+  return false;
+}
+
+int main()
+{
+  string target;
+  cout << target;
+}
