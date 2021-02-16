20f9b4e77f537978558ff6bb5077abcc2ec56f72
Spice-Z <jigenbakdaaan@gmail.com>
2021-02-16 22:27:24 +0900

    add

--- src/ABC049C.cpp
+++ src/ABC049C.cpp
@@ -1,27 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool check(string &target)
+string check(string &target)
 {
-
-  const vector<string> candidates = {"dreamer", "eraser", "dream", "erase"};
-  for (int i = 0; candidates.size();)
+  int origin_target_size = target.size();
+  vector<string> candidates(6);
+  candidates = {"dreameraser", "dreamerase", "dreamer", "eraser", "dream", "erase"};
+  int candidates_size = candidates.size();
+  for (int i = 0; i < candidates_size; i++)
   {
-    // target切り取り
-    candidates.at(i);
-
-    // check(target)
+    if (target.compare(0, candidates[i].size(), candidates[i]) == 0)
+    {
+      target.erase(0, candidates[i].size());
+      break;
+    }
   }
-
-  if (target.size() == 0)
+  int current_target_size = target.size();
+  if (origin_target_size != current_target_size)
   {
-    return true;
+    return check(target);
   }
-  return false;
+  return target;
 }
 
 int main()
 {
   string target;
-  cout << target;
+  cin >> target;
+  string result = check(target);
+  if (result.size() == 0)
+  {
+    cout << "YES";
+  }
+  else
+  {
+    cout << "NO";
+  }
+  return 0;
 }
